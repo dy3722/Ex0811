7a76188714a332fa55f92155266b1119227bbf47
dy3722 <dy3722@bs.amalnet.k12.il>
2025-09-18 00:10:27 +0300

    finish!

--- app/src/main/java/com/example/ex0811/CreditsActivity.java
+++ app/src/main/java/com/example/ex0811/CreditsActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex0811;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +12,29 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class CreditsActivity extends AppCompatActivity {
+    TextView tv;
+    Intent gi;
+    double resNum;
+    boolean isRes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
+
+        tv = findViewById(R.id.tv);
+        gi = getIntent();
+
+        isRes = gi.getBooleanExtra("isR",false);
+        resNum = gi.getDoubleExtra("res",0);
+
+        if (isRes)
+        {
+            tv.setText(resNum + "");
+        }
+    }
+
+    public void back(View view) {
+        finish();
     }
 }
--- app/src/main/java/com/example/ex0811/MainActivity.java
+++ app/src/main/java/com/example/ex0811/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex0811;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +12,152 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Intent si;
+    EditText et;
+    double num , resNum;
+    boolean isNum , isRes , areRes;
+    char op;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        si = new Intent(this, CreditsActivity.class);
+        et = findViewById(R.id.et);
+        op = 'X';
+        isRes = false;
+        isNum = false;
+        areRes = false;
+    }
+
+
+    public void plus(View view) {
+        if (et.getText().toString().matches("[^InfityaN]*") && !et.getText().toString().equals("") && (!et.getText().toString().contains(String.valueOf('E')) || (et.getText().toString().indexOf('E') != et.getText().toString().length() - 1 && et.getText().toString().indexOf('E') != 0))  && (!et.getText().toString().contains(String.valueOf('-')) || (et.getText().toString().indexOf('-') == 0) && et.getText().toString().indexOf('-') != et.getText().toString().length() - 1))
+        {
+            num = Double.parseDouble(et.getText().toString());
+            areRes = false;
+            if (isNum)
+            {
+                resNum = calc(resNum,op,Double.parseDouble(et.getText().toString()));
+            }
+            else
+            {
+                resNum = Double.parseDouble(et.getText().toString());
+                isNum = true;
+            }
+            op = '+';
+            et.setText("");
+        }
+    }
+
+    public void minus(View view) {
+        if (et.getText().toString().matches("[^InfityaN]*") && !et.getText().toString().equals("") && (!et.getText().toString().contains(String.valueOf('E')) || (et.getText().toString().indexOf('E') != et.getText().toString().length() - 1 && et.getText().toString().indexOf('E') != 0))  && (!et.getText().toString().contains(String.valueOf('-')) || (et.getText().toString().indexOf('-') == 0) && et.getText().toString().indexOf('-') != et.getText().toString().length() - 1))
+        {
+            num = Double.parseDouble(et.getText().toString());
+            areRes = false;
+            if (isNum)
+            {
+                resNum = calc(resNum,op,Double.parseDouble(et.getText().toString()));
+            }
+            else
+            {
+                resNum = Double.parseDouble(et.getText().toString());
+                isNum = true;
+            }
+            op = '-';
+            et.setText("");
+        }
+    }
+
+    public void multi(View view) {
+        if (et.getText().toString().matches("[^InfityaN]*") && !et.getText().toString().equals("") && (!et.getText().toString().contains(String.valueOf('E')) || (et.getText().toString().indexOf('E') != et.getText().toString().length() - 1 && et.getText().toString().indexOf('E') != 0)) && (!et.getText().toString().contains(String.valueOf('-')) || (et.getText().toString().indexOf('-') == 0) && et.getText().toString().indexOf('-') != et.getText().toString().length() - 1))
+        {
+            num = Double.parseDouble(et.getText().toString());
+            areRes = false;
+            if (isNum)
+            {
+                resNum = calc(resNum,op,Double.parseDouble(et.getText().toString()));
+            }
+            else
+            {
+                resNum = Double.parseDouble(et.getText().toString());
+                isNum = true;
+            }
+            op = '*';
+            et.setText("");
+        }
+    }
+
+    public void division(View view) {
+        if (et.getText().toString().matches("[^InfityaN]*") && !et.getText().toString().equals("") && (!et.getText().toString().contains(String.valueOf('E')) || (et.getText().toString().indexOf('E') != et.getText().toString().length() - 1 && et.getText().toString().indexOf('E') != 0)) && (!et.getText().toString().contains(String.valueOf('-')) || (et.getText().toString().indexOf('-') == 0) && et.getText().toString().indexOf('-') != et.getText().toString().length() - 1))
+        {
+            num = Double.parseDouble(et.getText().toString());
+            areRes = false;
+            if (isNum)
+            {
+                resNum = calc(resNum,op,Double.parseDouble(et.getText().toString()));
+            }
+            else
+            {
+                resNum = Double.parseDouble(et.getText().toString());
+                isNum = true;
+            }
+            op = '/';
+            et.setText("");
+        }
+    }
+
+    public void ac(View view)
+    {
+        et.setText("");
+        isNum = false;
+        op = 'X';
+    }
+
+    public void eq(View view) {
+        if (et.getText().toString().matches("[^InfityaN]*") && !et.getText().toString().equals("") && (!et.getText().toString().contains(String.valueOf('E')) || (et.getText().toString().indexOf('E') != et.getText().toString().length() - 1 && et.getText().toString().indexOf('E') != 0)) && (!et.getText().toString().contains(String.valueOf('-')) || (et.getText().toString().indexOf('-') == 0) && et.getText().toString().indexOf('-') != et.getText().toString().length() - 1))
+        {
+            isRes = true;
+            if (op == 'X')
+            {
+                resNum = Double.parseDouble(et.getText().toString());
+            }
+            else
+            {
+                if (areRes)
+                {
+                    resNum = calc(resNum,op,num);
+                }
+                else
+                {
+                    resNum = calc(resNum,op,Double.parseDouble(et.getText().toString()));
+                }
+            }
+            et.setText(resNum + "");
+            isNum = false;
+            areRes = true;
+        }
+
+    }
+
+    public void cred(View view) {
+        si.putExtra("isR",isRes);
+        si.putExtra("res",resNum);
+        startActivity(si);
+    }
+
+    private static double calc(double res, char op, double num) {
+        switch (op)
+        {
+            case '+':
+                return res + num;
+            case '-':
+                return res - num;
+            case '*':
+                return res * num;
+            default:
+                return res / num;
+        }
     }
 }
